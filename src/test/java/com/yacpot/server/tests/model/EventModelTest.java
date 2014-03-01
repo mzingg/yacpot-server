d863a5f4b1d824ce28cf6d24c4db2e4ffddd3404
Markus Zingg <mzingg@gmx.net>
2014-03-01 18:29:24 +0100

    Added further tests to timeline interval.

--- src/test/java/com/yacpot/server/tests/model/EventModelTest.java
+++ src/test/java/com/yacpot/server/tests/model/EventModelTest.java
@@ -5,15 +5,16 @@ import com.yacpot.server.model.Event;
 import com.yacpot.server.model.EventTimeline;
 import com.yacpot.server.model.SingleDateIncarnation;
 import com.yacpot.server.model.sort.EventModelComparator;
+import org.joda.time.LocalDate;
 import org.joda.time.LocalDateTime;
+import org.joda.time.LocalTime;
 import org.junit.Test;
 
 import java.util.Set;
 import java.util.TreeSet;
 
 import static com.yacpot.server.tests.ModelTestUtil.toJoinedString;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.*;
 
 public class EventModelTest {
 
@@ -68,9 +69,9 @@ public class EventModelTest {
 
   @Test
   public void testTimelineSortAllDatesAfterAnchor() {
-    LocalDateTime third = new LocalDateTime(2014, 3, 10, 10, 0);
-    LocalDateTime first = new LocalDateTime(2014, 1, 10, 10, 0);
-    LocalDateTime second = new LocalDateTime(2014, 2, 10, 10, 0);
+    LocalDateTime third = new LocalDateTime(2014, 1, 10, 10, 0);
+    LocalDateTime first = new LocalDateTime(2014, 2, 10, 10, 0);
+    LocalDateTime second = new LocalDateTime(2014, 3, 10, 10, 0);
 
     LocalDateTime anchor = new LocalDateTime(2013, 12, 10, 10, 0);
 
@@ -82,8 +83,8 @@ public class EventModelTest {
 
   @Test
   public void testTimelineSortAnchorBetweenDatesBefore() {
-    LocalDateTime second = new LocalDateTime(2014, 2, 10, 10, 0);
-    LocalDateTime first = new LocalDateTime(2014, 1, 10, 10, 0);
+    LocalDateTime second = new LocalDateTime(2014, 1, 10, 10, 0);
+    LocalDateTime first = new LocalDateTime(2014, 2, 10, 10, 0);
     LocalDateTime third = new LocalDateTime(2014, 3, 10, 10, 0);
 
     LocalDateTime anchor = new LocalDateTime(2014, 2, 9, 10, 0);
@@ -96,9 +97,9 @@ public class EventModelTest {
 
   @Test
   public void testTimelineSortAnchorBetweenDatesAfter() {
-    LocalDateTime third = new LocalDateTime(2014, 3, 10, 10, 0);
+    LocalDateTime third = new LocalDateTime(2014, 1, 10, 10, 0);
     LocalDateTime second = new LocalDateTime(2014, 2, 10, 10, 0);
-    LocalDateTime first = new LocalDateTime(2014, 1, 10, 10, 0);
+    LocalDateTime first = new LocalDateTime(2014, 3, 10, 10, 0);
 
     LocalDateTime anchor = new LocalDateTime(2014, 2, 11, 10, 0);
 
@@ -106,5 +107,30 @@ public class EventModelTest {
     testObj.incarnation(new SingleDateIncarnation(first)).incarnation(new SingleDateIncarnation(second)).incarnation(new SingleDateIncarnation(third));
 
     assertEquals(third, testObj.sortDate(anchor));
+  }
+
+  @Test(expected = IllegalArgumentException.class)
+  public void testIntervalThrowsExceptionWhenDatesAreNotConsecutive() throws Exception {
+    LocalDate intervalStart = new LocalDate(2014, 1, 10);
+    LocalDate intervalEnd = intervalStart.plusDays(2);
+
+    EventTimeline testObj = new EventTimeline();
+    testObj.incarnation(new DateIntervalIncarnation(intervalStart.toLocalDateTime(new LocalTime(0, 0)), intervalEnd.toLocalDateTime(new LocalTime(0, 0))));
+
+    testObj.hasIncarnationsDuring(intervalStart.plusDays(1), intervalStart);
+  }
+
+  @Test
+  public void testTakesPlaceBounds() {
+    LocalDate intervalStart = new LocalDate(2014, 1, 10);
+    LocalDate intervalEnd = intervalStart.plusDays(2);
+
+    EventTimeline testObj = new EventTimeline();
+    testObj.incarnation(new DateIntervalIncarnation(intervalStart.toLocalDateTime(new LocalTime(0, 0)), intervalEnd.toLocalDateTime(new LocalTime(0, 0))));
+
+    assertFalse(testObj.hasIncarnationsDuring(intervalStart.minusDays(1), intervalStart));
+    assertTrue(testObj.hasIncarnationsDuring(intervalStart, intervalStart.plusDays(5)));
+    assertTrue(testObj.hasIncarnationsDuring(intervalEnd.minusDays(10), intervalEnd));
+    assertFalse(testObj.hasIncarnationsDuring(intervalEnd.plusDays(1), intervalEnd.plusDays(2)));
   }
 }
